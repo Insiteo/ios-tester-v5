ece7e78a64007b2ef6f309005387694a7b49621d
Florian Lecoeuche <florian.lecoeuche@insiteo.com>
2019-06-14 10:36:10 +0200

    Create app

---
+++ ios-tester-v5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-tester-v5
+//
+//  Created by Lionel Rossignol on 14/06/2019.
+//  Copyright © 2019 Insiteo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
